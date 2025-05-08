25b4e4a764b986096e298278fc53b932ae2a64e0
BcRikko <s.shin1106@gmail.com>
2025-05-08 14:33:30 +0900

    rehype-mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
+import rehypeMermaid from 'rehype-mermaid'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,12 +16,17 @@ export default defineConfig({
     shikiConfig: {
       theme: 'dark-plus',
     },
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid', 'math'],
+    },
     rehypePlugins: [
       (...args) => rehypeKatex({
         ...args,
         output: 'mathml',
         strict: false,
       }),
+      rehypeMermaid,
     ],
   },
 })
